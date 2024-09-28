653b45e3ce1e01752065fd112777c09154db3e21
oxdeca <acedxo@oxdeca.com>
2024-09-28 18:11:38 -0400

    add nat

--- modules/gcp/net/main.tf
+++ modules/gcp/net/main.tf
@@ -7,16 +7,11 @@ locals {
   defaults = {
     "subnets" = false,
   }
-  subnets = [ for netk, netv in nets : [ for subnet in ] ]
 }
 
 resource "google_compute_network" "net" {
   for_each = { for net in var.nets : net.name => net }
 
   name                    = each.value.name
-  auto_create_subnetworks = local.defaults.subnets
-}
-
-resource "google_compute_subnetworks" "subnets" {
-  for_each = { for subnet in local.subnets }
+  auto_create_subnetworks = try( each.value.subnets, local.defaults.subnets )
 }
