b114c277d4f0914f320e8f7063074941cb67c3e4
oxdeca <acedxo@oxdeca.com>
2024-10-21 19:57:16 -0400

    module project: add check if oid or folder_id should be used.

--- modules/gcp/project/main.tf
+++ modules/gcp/project/main.tf
@@ -14,8 +14,8 @@ locals {
 resource "google_project" "prj" {
   name            = var.project.name
   project_id      = var.project.id
-  org_id          = var.project.oid
-  #folder_id      = var.project.folder
+  org_id          = var.folder_id   == null ? var.project.oid    : null
+  folder_id       = var.project.oid == null ? var.project.folder : null
   billing_account = var.project.billing
   labels          = var.project.labels
   deletion_policy = try( var.project.delete_policy, local.defaults.delete_policy )
