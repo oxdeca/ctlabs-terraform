813509b124624630990605b46740e88648e6922f
oxdeca <acedxo@oxdeca.com>
2024-12-18 11:06:06 -0500

    add sample project 5c

---
+++ 05c_service-vpc/gcp/main.tf
@@ -0,0 +1,16 @@
+# -----------------------------------------------------------------------------
+# File        : ctlabs-terraform/05c_service-vpc/gcp/main.tf
+# Description : terraform configuration to provision service project
+# -----------------------------------------------------------------------------
+
+locals { gcpconf = yamldecode( file("./gcp.conf.yml") ) }
+locals { config  = yamldecode( file("./config.yml") ) }
+
+
+module "secrets" {
+  source = "../../modules/gcp/ctlabs"
+
+  project = local.gcpconf.project
+  config  = local.config
+}
+
