f679c3b6f888b532b3f6893f34ad09eb796b5ed9
oxdeca <acedxo@oxdeca.com>
2025-01-15 22:47:34 -0500

    firewall-module: add features

--- modules/gcp/firewall/main.tf
+++ modules/gcp/firewall/main.tf
@@ -9,7 +9,8 @@ locals {
     "prio"     = 1000, 
     "action"   = "allow",
     "log"      = true,
-    "metadata" = "EXCLUDE_ALL_METADATA",
+    "metadata" = true,
+    "enable"   = true,
   }
   ingress = flatten( [ for netk, netv in var.firewall : [ for rule in netv.ingress: merge( { net_id = netk }, rule ) ] ] )
   egress  = flatten( [ for netk, netv in var.firewall : [ for rule in netv.egress : merge( { net_id = netk }, rule ) ] ] )
@@ -19,12 +20,14 @@ resource "google_compute_firewall" "ingress" {
   for_each = { for rule in local.ingress : rule.name => rule }
 
   direction     = "INGRESS"
+  project       = try( each.value.project, null)
   name          = each.value.name
   network       = each.value.net_id
   source_ranges = each.value.src
   priority      = try( each.value.prio, local.defaults.prio )
   target_tags   = try( each.value.tags, null )
   description   = try( each.value.desc, null )
+  disabled      = try( each.value.enable, local.defaults.enable) == false ? true : false
 
   dynamic allow {
     for_each = try( each.value.action, local.defaults.action ) == "allow" ? toset(["allow"]) : toset([])
@@ -45,7 +48,7 @@ resource "google_compute_firewall" "ingress" {
   dynamic log_config {
     for_each = try( each.value.log, local.defaults.log ) == true ? toset([try( each.value.log, local.defaults.log )]) : toset([])
     content {
-      metadata = try( each.value.metadata, local.defaults.metadata )
+      metadata = try( each.value.metadata, local.defaults.metadata ) == true ? "INCLUDE_ALL_METADATA" : "EXCLUDE_ALL_METADATA"
     }
   }
 }
@@ -54,12 +57,14 @@ resource "google_compute_firewall" "egress" {
   for_each = { for rule in local.egress : rule.name => rule }
 
   direction          = "EGRESS"
+  project            = try( each.value.project, null)
   name               = each.value.name
   network            = each.value.net_id
   destination_ranges = each.value.dst
   priority           = try( each.value.prio, local.defaults.prio )
   target_tags        = try( each.value.tags, null )
   description        = try( each.value.desc, null )
+  disabled           = try( each.value.enable, local.defaults.enable) == false ? true : false
 
   dynamic allow {
     for_each = try( each.value.action, local.defaults.action )  == "allow" ? toset(["allow"]) : toset([])
@@ -80,7 +85,7 @@ resource "google_compute_firewall" "egress" {
   dynamic log_config {
     for_each = try( each.value.log, local.defaults.log ) == true ? toset([try( each.value.log, local.defaults.log )]) : toset([])
     content {
-      metadata = try( each.value.metadata, local.defaults.metadata )
+      metadata = try( each.value.metadata, local.defaults.metadata ) == true ? "INCLUDE_ALL_METADATA" : "EXCLUDE_ALL_METADATA"
     }
   }
 }
