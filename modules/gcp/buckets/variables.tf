2f3652df94d67b7025c5b194b6b1c383268d0363
oxdeca <acedxo@oxdeca.com>
2025-01-27 23:57:52 -0500

    buckets-module: validate

--- modules/gcp/buckets/variables.tf
+++ modules/gcp/buckets/variables.tf
@@ -7,17 +7,17 @@ variable project { type = any }
 variable buckets { 
   type = any
 
-  validate {
+  validation {
     condition     = all(map(values(var.buckets))[*].name != null)
     error_message = "Every bucket must have a name."
   }
   
-  validate {
+  validation {
     condition     = all(map(values(var.buckets))[*].class in ["STANDARD", "MULTI_REGIONAL", "REGIONAL", "NEARLINE", "COLDLINE", "ARCHIVE", "AUTO", "AUTO_ARCHIVE", "AUTO_NEARLINE" ] )
     error_message = "Invalid storage class specified"
   }
   
-  validate {
+  validation {
     condition     = all(map(values(var.buckets))[*].private in [ true, false ]
     error_message = "private if set to 'true' else public!"
   }
