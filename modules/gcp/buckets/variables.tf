6e3c0d5f386d4163847b5ec3f21f7d2637ac809b
oxdeca <acedxo@oxdeca.com>
2025-01-27 23:55:34 -0500

    buckets-module: validate

--- modules/gcp/buckets/variables.tf
+++ modules/gcp/buckets/variables.tf
@@ -4,21 +4,22 @@
 # -----------------------------------------------------------------------------
 
 variable project { type = any }
-variable buckets { type = any
+variable buckets { 
+  type = any
 
-validate {
-  condition     = all(map(values(var.buckets))[*].name != null)
-  error_message = "Every bucket must have a name."
-}
-
-validate {
-  condition     = all(map(values(var.buckets))[*].class in ["STANDARD", "MULTI_REGIONAL", "REGIONAL", "NEARLINE", "COLDLINE", "ARCHIVE", "AUTO", "AUTO_ARCHIVE", "AUTO_NEARLINE" ] )
-  error_message = "Invalid storage class specified"
-}
-
-validate {
-  condition     = all(map(values(var.buckets))[*].private in [ true, false ]
-  error_message = "private if set to 'true' else public!"
-}
+  validate {
+    condition     = all(map(values(var.buckets))[*].name != null)
+    error_message = "Every bucket must have a name."
+  }
+  
+  validate {
+    condition     = all(map(values(var.buckets))[*].class in ["STANDARD", "MULTI_REGIONAL", "REGIONAL", "NEARLINE", "COLDLINE", "ARCHIVE", "AUTO", "AUTO_ARCHIVE", "AUTO_NEARLINE" ] )
+    error_message = "Invalid storage class specified"
+  }
+  
+  validate {
+    condition     = all(map(values(var.buckets))[*].private in [ true, false ]
+    error_message = "private if set to 'true' else public!"
+  }
 
 }
