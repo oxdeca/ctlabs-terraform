102aedb2ef98e5c58a94b37c693dcbd298e0b665
oxdeca <acedxo@oxdeca.com>
2025-03-09 14:10:51 -0400

    buckets module

--- modules/gcp/buckets/variables.tf
+++ modules/gcp/buckets/variables.tf
@@ -4,12 +4,4 @@
 # -----------------------------------------------------------------------------
 
 variable project { type = any }
-variable buckets {
-  type        = any
-  description = "Configuration for GCP buckets."
-
-  validation {
-    condition     = all(map(keys(var.buckets))[*].name != null)
-    error_message = "Every bucket must have a 'name' field."
-  }
-}
+variable buckets { type = any }
