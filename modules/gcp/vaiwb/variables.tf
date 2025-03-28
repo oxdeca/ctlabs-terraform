dc9e2ca3db280c08e6b386b8607087e626350d6a
oxdeca <acedxo@oxdeca.com>
2025-03-27 23:47:57 -0400

    vertex-ai-module

---
+++ modules/gcp/vaiwb/variables.tf
@@ -0,0 +1,7 @@
+# -----------------------------------------------------------------------------
+# File        : ctlabs-terraform/modules/gcp/vaiwb/variables.tf
+# Description : vertex-ai workbench module variables
+# -----------------------------------------------------------------------------
+
+variable wbs     { type = any }
+variable project { type = any }
