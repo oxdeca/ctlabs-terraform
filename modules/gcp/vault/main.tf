c717d75b94e78a99e3c048f8338870ac45df313e
oxdeca <acedxo@oxdeca.com>
2026-02-12 08:55:30 -0500

    vault module

--- modules/gcp/vault/main.tf
+++ modules/gcp/vault/main.tf
@@ -34,3 +34,35 @@ output "secrets" {
   value     = { for key, secret in ephemeral.vault_kv_secret_v2.secrets : key => secret.data }
   ephemeral = true
 }
+
+
+# -----------------------------------------------------------------------------
+# Google Secret Manager (GSM) - get approle credentials
+# -----------------------------------------------------------------------------
+#
+#ephemeral "google_secret_manager_secret_version" "role_id" {
+#  secret  = var.vault.gsm.role_id
+#  project = var.vault.gsm.project
+#}
+#
+#ephemeral "google_secret_manager_secret_version" "secret_id" {
+#  secret  = var.vault.gsm.secret_id
+#  project = var.vault.gsm.project
+#}
+
+# -----------------------------------------------------------------------------
+# Hashicorp Vault - login
+# -----------------------------------------------------------------------------
+#
+#provider "vault" {
+#  address = var.vault.url
+#  skip_tls_verify = !var.vault.ssl_verify
+#
+#  auth_login {
+#    path = "auth/${var.vault.approle}/login"
+#    parameters = {
+#      role_id   = ephemeral.google_secret_manager_secret_version.role_id.secret_data
+#      secret_id = ephemeral.google_secret_manager_secret_version.secret_id.secret_data
+#    }
+#  }
+#}
--- modules/gcp/vault/variables.tf
+++ modules/gcp/vault/variables.tf
@@ -3,5 +3,21 @@
 # License : MIT
 # -----------------------------------------------------------------------------
 
-variable vault   { type = any }
-variable project { type = any }
+variable "vault" {
+  type = object({
+    secrets = list(object({
+      name = string
+      path = string
+    }))
+    url        = string
+    mount      = string
+    env        = optional(string)
+    approle    = optional(string)
+    ssl_verify = optional(bool, true)
+    gsm = optional(object({
+      project   = optional(string)
+      role_id   = optional(string)
+      secret_id = optional(string)
+    }), {})
+  })
+}
