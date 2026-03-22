f492e3f18dacc806385442415697644876ad15f1
oxdeca <acedxo@oxdeca.com>
2026-03-22 13:48:02 -0400

    vm module

--- modules/gcp/vm/main.tf
+++ modules/gcp/vm/main.tf
@@ -92,7 +92,7 @@ resource "google_compute_instance" "vm" {
 
   network_interface {
     subnetwork         = each.value.network
-    subnetwork_project = try(var.project.host_project, null)
+    #subnetwork_project = try(var.project.host_project, null)
     network_ip         = each.value.ipv4
 
     dynamic "access_config" {
